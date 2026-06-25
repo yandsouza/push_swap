bd9e3e8e577b058e5d243fefd7e507d3b959ebc2
Yann D'Souza <yannssouza@outlook.com>
2026-06-25 01:47:50 -0300

    rotate fixed

--- ops/reverse.c
+++ ops/reverse.c
@@ -6,36 +6,23 @@
 /*   By: ynascime <yannssouza@outlook.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 17:36:35 by ynascime          #+#    #+#             */
-/*   Updated: 2026/06/23 18:39:49 by ynascime         ###   ########.fr       */
+/*   Updated: 2026/06/25 01:46:48 by ynascime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	reverse(t_stack *stack)
+void    reverse(t_stack *stack)
 {
-	t_node	*node;
+    t_node  *node;
 
-	if (!stack)
-		return ;
-	node = stack->bottom;
-	node->next = stack->top;
-	stack->top = node;
-	stack->bottom = node->prev;
-	stack->top->prev = NULL;
-	stack->bottom->next = NULL;
+    if (!stack)
+        return ;
+    node = stack->bottom;
+    stack->bottom = node->prev;
+    stack->bottom->next = NULL;
+    node->next = stack->top;
+    stack->top->prev = node;
+    node->prev = NULL;
+    stack->top = node;
 }
-
-//void	old_rotate(t_stack *stack)
-//{
-//	t_node	*node;
-//
-//	node = stack->top;
-//	stack->top = stack->bottom;
-//	stack->bottom = node;
-//	stack->top->next = node->next;
-//	stack->bottom->prev = stack->top->prev;
-//	stack->bottom->prev->next = stack->bottom;
-//	stack->top->prev = NULL;
-//	stack->bottom->next = NULL;
-//}
--- ops/rotate.c
+++ ops/rotate.c
@@ -6,37 +6,23 @@
 /*   By: ynascime <yannssouza@outlook.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/20 15:21:44 by ynascime          #+#    #+#             */
-/*   Updated: 2026/06/23 18:48:50 by ynascime         ###   ########.fr       */
+/*   Updated: 2026/06/25 01:44:38 by ynascime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	rotate(t_stack *stack)
+void    rotate(t_stack *stack)
 {
-	t_node	*node;
+    t_node  *node;
 
-	if (!stack)
-		return ;
-	node = stack->top;
-	node->prev = stack->bottom->prev;
-	stack->bottom->next = node;
-	stack->bottom = node;
-	stack->top = node->next;
-	stack->top->prev = NULL;
-	stack->bottom->next = NULL;
+    if (!stack)
+        return ;
+    node = stack->top;
+    stack->top = node->next;
+    stack->top->prev = NULL;
+    node->prev = stack->bottom;
+    stack->bottom->next = node;
+    node->next = NULL;
+    stack->bottom = node;
 }
-
-//void	old_rotate(t_stack *stack)
-//{
-//	t_node	*node;
-//
-//	node = stack->top;
-//	stack->top = stack->bottom;
-//	stack->bottom = node;
-//	stack->top->next = node->next;
-//	stack->bottom->prev = stack->top->prev;
-//	stack->bottom->prev->next = stack->bottom;
-//	stack->top->prev = NULL;
-//	stack->bottom->next = NULL;
-//}
